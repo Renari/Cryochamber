6613acd632f5a76e06f47356d64dc1ebf0cb0ae5
Renari <renari@arimil.com>
2013-07-08 23:59:54 -0400

    version changed to 0.2

--- src/com/arimil/cryochamber/lib/References.java
+++ src/com/arimil/cryochamber/lib/References.java
@@ -9,7 +9,7 @@ public class References {
     //General mod related constants
     public static final String MOD_ID = "Cryochamber";
     public static final String MOD_NAME = "Cryochamber";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     public static final String DEPENDENCIES = "required-after:IC2";
     
     public static int nausea_duration = 40;
